ee2bcd9206aa3de0eea357062b67a366e181d38b
Jonathan Soo <jcsoo@agora.com>
2014-07-29 08:59:38 -0400

    Add environment configuration for server / port

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 import (
   "log"
+  "os"
   "strings"
   "regexp"
   "fmt"
@@ -65,5 +66,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":5000", nil)
+    http.ListenAndServe(os.Getenv("SERVER_HOST")+":"+os.Getenv("SERVER_PORT"), nil)
 }
