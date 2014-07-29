32772535bc54e5dddae7b64658fd9d634f164f86
Jonathan Soo <jcsoo@agora.com>
2014-07-29 08:48:58 -0400

    Add regular expression matches

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 import (
   "log"
   "strings"
+  "regexp"
   "fmt"
   "net"
   "net/http"
@@ -27,16 +28,39 @@ func lookupPeersByDomain(domain string) (peers []string) {
 func handler(w http.ResponseWriter, r *http.Request) {
   peers := lookupPeersByDomain("lightreading.systems")
   client := etcd.NewClient(peers)
-  key := "/config/redirects/"+r.Host+"/"+"_"
+  key := "/config/redirects/"+r.Host
   response, err := client.Get(key, false, false)
-  if err == nil {
-    target := strings.Replace(response.Node.Value,"$1",r.RequestURI,1)
-    log.Printf("http://%s%s -> %s\n", r.Host, r.RequestURI, target)
-    http.Redirect(w, r, "http://www.google.com/",302)
-  } else {
-    log.Println(err)
+  if err != nil {
+    log.Printf("Error looking up host: %s\n",err)
     http.NotFound(w, r)
+    return
   }
+  lines := strings.Split(response.Node.Value,"\n")
+  for _, line := range lines {
+    fields := strings.Split(line," ")
+    if len(fields) < 1 {
+      continue
+    }
+    pattern := fields[0]
+    template := fields[1]
+    re, err := regexp.Compile(pattern)
+    if err != nil {
+      log.Printf("Error compiling regular expression %s: %s", pattern, err)
+      continue
+    }
+    if re.MatchString(r.RequestURI) == true {
+      target := []byte{}
+      for _, s := range re.FindAllStringSubmatchIndex(r.RequestURI,-1) {
+        target = re.ExpandString(target, template, r.RequestURI, s)
+	    }
+
+      log.Printf("%s matched URI %s", pattern, r.RequestURI)
+      log.Printf("http://%s%s -> %s\n", r.Host, r.RequestURI, target)
+      http.Redirect(w, r, string(target) ,302)
+      return
+    }
+  }
+  http.NotFound(w, r)
 }
 
 func main() {
